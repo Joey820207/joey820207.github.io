44fe98b73dcc82a9ebad2c451247484f7ed4838b
Joey820207 <cn.zouwei@gmail.com>
2020-03-27 21:39:20 +0100

    Site updated: 2020-03-27 21:39:14

--- js/src/usercodecheck.js
+++ js/src/usercodecheck.js
@@ -1,13 +1,16 @@
 var OriginTitle = document.title;
 var usercode=document.getElementById('usercode');
 var btncheck=document.getElementById('codecheck');
-
+alert(usercode.value);
 function codecheck() {
-	if (OriginTitle.value =="js-integrate"){
-		if(usercode.value == "tiantian"){
-			warn("Welcome back");
-		} else {
-			console.warn("pls check your code");
-		}
-	}
+	// alert(OriginTitle.value);
+	// alert(usercode.value);
+	// if (OriginTitle.value =="js-integrate"){
+	// 	if(usercode.value == "tiantian"){
+	// 		warn("Welcome back");
+	// 	} else {
+	// 		console.warn("pls check your code");
+	// 	}
+	// }
+	prompt("Test prompt window","tiantian");
 }
